31d3b8c34ffb7a094346c96e3b1a6220097fe235
K-PAVI-THRA <pavithra.karthigayan@gmail.com>
2024-12-30 14:30:00 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,4 @@
+from sklearn.model_selection import train_test_split
+
+# Split data into training and testing sets (80% for training, 20% for testing)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
